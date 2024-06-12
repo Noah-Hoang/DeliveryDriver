feb830126b45d8d6222e614f60c9a2f19abf4129
0shakey <118243453+0shakey@users.noreply.github.com>
2024-06-11 18:00:13 -0700

    Fixing class names

--- Assets/Scripts/Teacher/Driver_T.cs
+++ Assets/Scripts/Teacher/Driver_T.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Driver : MonoBehaviour
+public class Driver_T : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
