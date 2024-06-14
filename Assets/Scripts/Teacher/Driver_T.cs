c8f742afdf70596c3a1224c30b9cdc345f0b88f8
Je'Don Carter <jedon007@yahoo.com>
2024-06-13 17:50:36 -0700

    Added drivable car, coded steering, made art into sprites, redesined capsle into car

--- Assets/Scripts/Teacher/Driver_T.cs
+++ Assets/Scripts/Teacher/Driver_T.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Driver_T : MonoBehaviour
 {
+    public float moveSpeed = 0.1f;
+    public float steering = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //moveSpeed = 0.0f;
+        //steering = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Steer();
+    }
+
+    public void Steer()
+    { 
+        float steerAmount = Input.GetAxis("Horizontal") * steering * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    moveSpeed = 0.1f;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.W))
+        //{
+        //    moveSpeed = 0.0f;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    steering = 0.5f;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.A))
+        //{
+        //    steering = 0.0f;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    moveSpeed = -0.1f;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.S))
+        //{
+        //    moveSpeed = 0.0f;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    steering = -0.5f;
+        //}
+        //else if (Input.GetKeyUp(KeyCode.D))
+        //{
+        //    steering = 0.0f;
+        //}
+
+        //transform.Rotate(0, 0, steering);
+        //transform.Translate(0, moveSpeed, 0);
     }
 }
