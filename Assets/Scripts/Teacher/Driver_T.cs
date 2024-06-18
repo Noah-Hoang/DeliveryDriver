fce769d12596391d91a67727911af3a08d647730
Je'Don Carter <jedon007@yahoo.com>
2024-06-18 16:10:23 -0700

    Update Driver_T.cs

--- Assets/Scripts/Teacher/Driver_T.cs
+++ Assets/Scripts/Teacher/Driver_T.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver_T : MonoBehaviour
 {
-    public float moveSpeed = 0.1f;
-    public float steering = 10;
+    public float moveSpeed = 10.0f;
+    public float steering = 100.0f;
 
     // Start is called before the first frame update
     void Start()
